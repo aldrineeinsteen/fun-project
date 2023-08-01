d30ed199fc135c6518623dbd05d2cc381a8182ca
Aldrine Einsteen <aldrine.einsteen@hotmail.com>
2023-08-01 13:13:40 +0100

    merge to analyse code (#16)
    * adding git user creds
    
    * adding PAT as secret
    
    * adding support for git bot
    
    * adding support for openjdk
    
    * switching temurin
    
    * trying again with GITHUB_TOKEN
    
    * attempting actions with contents write
    
    * trying again with PAT to deploy
    
    * updating version
    
    * adding support for next version
    
    * trying with a new version 1.1.0
    
    * adding tag and releaseVersion
    
    * trying dev build with PAT
    
    * splitting prepare and perform
    
    * adding support for description
    
    * adding additional documentation
    
    * removing merge conflicts
    
    * removing merge conflicts
    
    * adding terminal support

---
+++ src/main/java/com/aldrineeinsteen/fun/options/SignatureSelector.java
@@ -0,0 +1,68 @@
+package com.aldrineeinsteen.fun.options;
+
+import org.jline.terminal.Terminal;
+import org.yaml.snakeyaml.Yaml;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class SignatureSelector implements Runnable{
+    private final List<String> signatures;
+    private final Random random = new Random();
+
+    private Terminal globalTerminal;
+
+    public SignatureSelector(Terminal terminal) throws IOException {
+        globalTerminal = terminal;
+        Yaml yaml = new Yaml();
+        Map<String, Object> yamlData = yaml.load(getClass().getClassLoader().getResourceAsStream("signatures.yaml"));
+        signatures = ((List<Map<String, String>>) yamlData.get("options")).stream()
+                .map(option -> option.get("signature"))
+                .collect(Collectors.toList());
+
+//        robot.addKeyListener(new KeyListener() {
+//            @Override
+//            public void keyPressed(KeyEvent e) {
+//                if (e.getKeyCode() == KeyEvent.VK_S && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
+//                    if (signatures != null && !signatures.isEmpty()) {
+//                        String signature = signatures.get(random.nextInt(signatures.size()));
+//                        System.out.println(signature);
+//                    }
+//                }
+//            }
+//
+//            @Override
+//            public void keyReleased(KeyEvent e) {}
+//
+//            @Override
+//            public void keyTyped(KeyEvent e) {}
+//        });
+    }
+
+    public String getRandomSignature() {
+        if (signatures != null && !signatures.isEmpty()) {
+            return signatures.get(random.nextInt(signatures.size()));
+        }
+        return null;
+    }
+
+    @Override
+    public void run() {
+        globalTerminal.writer().println("Starting the terminal...");
+        int read;
+        while (true) {
+            try {
+                if (!((read = globalTerminal.reader().read()) != -1)) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            char ch = (char) read;
+            System.out.println("Key pressed: " + ch);
+        }
+//
+    }
+}
--- start.cmd
+++ start.cmd
@@ -6,13 +6,13 @@ IF "%end_time%"=="" SET end_time=17:00
 
 IF EXIST %jarfile% (
     echo Running fun-project.jar
-    java -jar %jarfile% --end-time %end_time% --keep-alive
+    java -jar %jarfile% --end-time %end_time% --keep-alive --signature
 ) ELSE (
     echo fun-project.jar not found, building with Maven
     mvnw clean install
     IF EXIST %jarfile% (
         echo Running fun-project.jar
-        java -jar %jarfile% --end-time %end_time% --keep-alive
+        java -jar %jarfile% --end-time %end_time% --keep-alive --signature
     ) ELSE (
         echo Error building jar file
         exit /b 1
