cd982e2216af4ecf163ab9e341cd8213ab544673
Aldrine Einsteen <aldrine.einsteen@hotmail.com>
2024-11-11 19:02:25 +0000

    adding support for start defaults

--- start.cmd
+++ start.cmd
@@ -1,18 +1,47 @@
 @echo off
-SET jarfile=target\fun-project.jar
-SET libFolder=target\lib\*
-SET pluginFolder=target\plugins\*
 
-IF EXIST %jarfile% (
+set "jarfile=target\fun-project.jar"
+set "libFolder=target\lib\*"
+set "pluginFolder=target\plugins\*"
+
+REM Initialize optional parameters with defaults
+set "signature=--signature"
+set "keep_alive=--keep-alive"
+set "end_time="
+
+REM Parse optional arguments
+:parse_args
+if "%~1"=="" goto after_args
+if "%~1"=="--signature" (
+    set "signature=--signature"
+    shift
+    goto parse_args
+)
+if "%~1"=="--keep-alive" (
+    set "keep_alive=--keep-alive"
+    shift
+    goto parse_args
+)
+if "%~1"=="--end-time" (
+    set "end_time=--end-time %~2"
+    shift
+    shift
+    goto parse_args
+)
+shift
+goto parse_args
+
+:after_args
+if exist "%jarfile%" (
     echo Running fun-project.jar
-    java -cp "%jarfile%;%libFolder%;%pluginFolder%" com.aldrineeinsteen.fun.Main --keep-alive --signature
-) ELSE (
+    java -cp "%libFolder%;%pluginFolder%;%jarfile%" com.aldrineeinsteen.fun.Main %signature% %keep_alive% %end_time%
+) else (
     echo fun-project.jar not found, building with Maven
-    mvnw clean install
-    IF EXIST %jarfile% (
+    call mvnw.cmd clean install
+    if exist "%jarfile%" (
         echo Running fun-project.jar
-        java -cp "%jarfile%;%libFolder%;%pluginFolder%" com.aldrineeinsteen.fun.Main --keep-alive --signature
-    ) ELSE (
+        java -cp "%libFolder%;%pluginFolder%;%jarfile%" com.aldrineeinsteen.fun.Main %signature% %keep_alive% %end_time%
+    ) else (
         echo Error building jar file
         exit /b 1
     )
